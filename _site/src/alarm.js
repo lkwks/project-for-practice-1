1d77f29dc852699b5c37e1033bb1f718423036a5
lkwks <69514453+lkwks@users.noreply.github.com>
2021-10-10 22:32:00 +0900

    일부 수정함

--- _site/src/alarm.js
+++ _site/src/alarm.js
@@ -1,33 +1,50 @@
-import {addNewMemoFunction} from "./memo.js";
+import {backButton, newButton, text_info, turnOffAll} from "./home.js";
 
-const new_alarm_node = document.getElementById("new-alarm");
+let alarms = null;
 
-function drawAlarmContent(text_info)
+export default function drawAlarmContent()
 {
-    for (let elem of document.getElementById("page-content").children)
-        elem.style.display = 'none';
+    turnOffAll();
     document.getElementById("alarm-content").style.display = 'block';
-    new_alarm_node.style.display = 'none';
+    backButton.style.display = 'block';
+    newButton.style.display = 'block';    
+}
 
-    document.getElementById("backButton").firstChild.style.display = 'block';
-    document.getElementById("newButton").firstChild.style.display = 'block';
-    document.getElementById("newButton").firstChild.addEventListener("click", addNewAlarmFunction);
-    document.getElementById("newButton").firstChild.removeEventListener("click", addNewMemoFunction);
+export function makeSaveButton()
+{
+    document.getElementById("saveButton").textContent = text_info["SaveButton"];
+    document.getElementById("saveButton").addEventListener("click", addNewAlarm);
+}
+
+export function addNewAlarm()
+{
+    let new_alarm_time = parseInt(document.getElementById("new-minute").value) + document.getElementById("new-hour").value * 60;
+    if (parseInt(document.getElementById("new-ampm").value) === 1)
+        new_alarm_time += 720;
+    if (parseInt(document.getElementById("new-hour").value) === 12)
+        new_alarm_time -= 720;
     
-    
-    const alarm_list = document.createElement("ul");
+    const new_alarms = alarms === null? new Array() : alarms;
+    new_alarms.unshift(new_alarm_time);
+    localStorage.setItem("alarms", JSON.stringify(new_alarms));
+    makeAlarmContent();
+    drawAlarmContent();
+}
+
+export function makeAlarmContent()
+{
     document.getElementById("alarm-list").innerHTML = '';
-    document.getElementById("alarm-list").appendChild(alarm_list);
-    const alarms = JSON.parse(localStorage.getItem("alarms"));
+    alarms = JSON.parse(localStorage.getItem("alarms"));
     if (alarms !== null)
         alarms.forEach((elem, idx, arr) =>
         {
             const alarm_elem = document.createElement("li");
-            alarm_list.appendChild(alarm_elem);
+            document.getElementById("alarm-list").appendChild(alarm_elem);
             alarm_elem.classList.add("alarm-element");
             const ampm = elem < 720 ? "오전" : "오후";
-            let hour = elem < 780 ? Math.floor(elem/60) : Math.floor(elem/60) -12;
-            if (elem < 60) hour = 12;
+            let hour = Math.floor(elem/60);
+            if (hour > 12) hour -= 12;
+            if (hour === 0) hour = 12;
             alarm_elem.textContent = `${ampm} ${hour}시 ${(elem%60)}분`;
 
             const delete_button = document.createElement("button");
@@ -38,31 +55,7 @@ function drawAlarmContent(text_info)
             {
                 arr.splice(idx, 1);
                 localStorage.setItem("alarms", JSON.stringify(arr));
-                drawAlarmContent(text_info);
+                drawAlarmContent();
             });
         });
 }
-
-function addNewAlarmFunction()
-{
-    new_alarm_node.style.display = 'block';
-}
-
-function addNewAlarm(text_info)
-{
-    const alarms = localStorage.getItem("alarms");
-    const new_alarms = alarms === null? new Array() : JSON.parse(alarms);
-    
-    let new_alarm_time = parseInt(document.getElementById("new-minute").value);
-    if (document.getElementById("new-ampm").value == 1 || (document.getElementById("new-ampm").value == 0 && parseInt(document.getElementById("new-hour").value) !== 12))
-        new_alarm_time += document.getElementById("new-hour").value * 60;
-    if (document.getElementById("new-ampm").value == 1 && parseInt(document.getElementById("new-hour").value) !== 12)
-        new_alarm_time += 720;
-    
-    new_alarms.unshift(new_alarm_time);
-    localStorage.setItem("alarms", JSON.stringify(new_alarms));
-    new_alarm_node.style.display = 'none';
-    drawAlarmContent(text_info);
-}
-
-export {drawAlarmContent as default, addNewAlarm, addNewAlarmFunction};
--- src/memo.js
+++ src/memo.js
@@ -1,49 +1,43 @@
-import {addNewAlarmFunction} from "./alarm.js";
+import {backButton, newButton, text_info, turnOffAll} from "./home.js";
 
-const new_memo_node = document.getElementById("memo-content").getElementsByTagName("input")[0];
-let now_clicked = null;
+let now_clicked = null, memos = null;
 
-export function addNewMemoFunction()
+export function makeMemoInput()
 {
-    new_memo_node.style.display = 'inline';
+    document.getElementById("new-memo").setAttribute("placeholder", text_info["MemoPlaceholder"]);
+    document.getElementById("new-memo").addEventListener("keyup", event=> addNewMemo(event.key));
 }
 
-export function addNewMemo(key, text_info)
+export function addNewMemo(key)
 {
     if (key == "Enter")
     {
-        const memos = localStorage.getItem("memos");
-        const new_memos = memos === null? new Array() : JSON.parse(memos);
-        new_memos.unshift(new_memo_node.value);
-        new_memo_node.value = '';
+        const new_memos = memos === null? new Array() : memos;
+        new_memos.unshift(document.getElementById("new-memo").value);
+        document.getElementById("new-memo").value = '';
         localStorage.setItem("memos", JSON.stringify(new_memos));
-        new_memo_node.style.display = 'none';
-        drawMemoContent(text_info);
+        makeMemoContent();
+        drawMemoContent();
     }
 }
 
-export default function drawMemoContent(text_info)
+export default function drawMemoContent()
 {
-    for (let elem of document.getElementById("page-content").children)
-        elem.style.display = 'none';
+    turnOffAll();
     document.getElementById("memo-content").style.display = 'block';
-    document.getElementById("memo-list").innerHTML = '';
-    new_memo_node.style.display = 'none';
+    backButton.style.display = 'block';
+    newButton.style.display = 'block';    
+}
 
-    document.getElementById("backButton").firstChild.style.display = 'block';
-    document.getElementById("newButton").firstChild.style.display = 'block';
-    document.getElementById("newButton").firstChild.addEventListener("click", addNewMemoFunction);
-    document.getElementById("newButton").firstChild.removeEventListener("click", addNewAlarmFunction);
-    
-    
-    const memo_list = document.createElement("ul");
-    document.getElementById("memo-list").appendChild(memo_list);
-    const memos = JSON.parse(localStorage.getItem("memos"));
+export function makeMemoContent()
+{
+    document.getElementById("memo-list").innerHTML = '';    
+    memos = JSON.parse(localStorage.getItem("memos"));
     if (memos !== null)
         memos.forEach(elem =>
         {
             const memo_elem = document.createElement("li");
-            memo_list.appendChild(memo_elem);
+            document.getElementById("memo-list").appendChild(memo_elem);
             memo_elem.textContent = elem;
             memo_elem.classList.add("memo-element");
             memo_elem.addEventListener("click", _=> 
