a035ecb4eb29d1aa27730688abf8675ed4fe55c9
lkwks <69514453+lkwks@users.noreply.github.com>
2021-10-20 01:47:57 +0900

    Update awesomespa.js

--- src/awesomespa.js
+++ src/awesomespa.js
@@ -88,6 +88,7 @@ class App {
         
         if (newContent === this.homeContent)
         {
+            console.log(this.alarmContent);
             if (this.usingNewButton)
                 this.newButton.toggle();
             if (this.alarmContent.newAlarm.target.classList.contains("hide") === false)
