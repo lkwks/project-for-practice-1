a58ea93588b9ff0da8b1a8d04b16a84532f399f2
lkwks <69514453+lkwks@users.noreply.github.com>
2021-10-19 23:33:01 +0900

    Update album.js

--- src/album.js
+++ src/album.js
@@ -5,7 +5,20 @@ export default class Album
         this.AppObj = AppObj;
         this.target = AppObj.albumContentNode;
         this.albumList = this.target.querySelector("ul");
+        this.albumViewFrame = this.target.querySelector("#album-view-frame");
         this.now_clicked=null;
+        this.target.addEventListener("click", event=>
+        {
+            if (event.target.nodeName === "LI")
+            {
+                if (this.now_clicked !== null)
+                    this.now_clicked.classList.remove("album-clicked");
+                event.target.classList.add("album-clicked");
+                this.albumViewFrame.style.backgroundImage = event.target.style.backgroundImage;
+                this.albumViewFrame.style.backgroundPosition = `center center`;
+                this.now_clicked = event.target;
+            }
+        });
         this.render();
     }
 
@@ -14,32 +27,21 @@ export default class Album
         this.target.classList.toggle("hide");
     }
     
+    makeListItem(elem)
+    {
+        const listItem = document.createElement("li");
+        listItem.classList.add("album-img-frame");
+        listItem.style.backgroundImage = `url(./album/${elem})`;
+        return listItem;
+    }
+    
     render()
     {
         fetch("./album/file_list.json")
             .then(response => response.json())
             .then(file_list =>
             {
-                file_list.forEach(elem => 
-                {
-                    const img_frame = document.createElement("li");
-                    this.albumList.appendChild(img_frame);
-                    img_frame.classList.add("album-img-frame");
-                    img_frame.style.backgroundImage = `url(./album/${elem})`;
-                });
-                
-                this.target.addEventListener("click", event=>
-                {
-                    if (event.target.nodeName === "LI")
-                    {
-                        if (this.now_clicked !== null)
-                            this.now_clicked.classList.remove("album-clicked");
-                        event.target.classList.add("album-clicked");
-                        this.target.querySelector("#album-view-frame").style.backgroundImage = event.target.style.backgroundImage;
-                        this.target.querySelector("#album-view-frame").style.backgroundPosition = `center center`;
-                        this.now_clicked = event.target;
-                    }
-                });
+                this.albumList.innerHTML = file_list.map(elem => this.makeListItem(elem)).join("");                
             }); 
     }
 }
